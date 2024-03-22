2ea7213ca9c9c8521baeca0c899c8d826c4320c4
ishaik <ibrahim.shaik@altnsolutions.co.uk>
2024-03-22 11:43:19 +0530

    Commit

---
+++ CICD_DB/dbo/Tables/Customer.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Customer] (
+    [CustomerID] INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName]  NVARCHAR (50)  NOT NULL,
+    [LastName]   NVARCHAR (50)  NOT NULL,
+    [Email]      NVARCHAR (100) NULL,
+    [Phone]      NVARCHAR (20)  NULL,
+    [Address]    NVARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([CustomerID] ASC)
+);
+
